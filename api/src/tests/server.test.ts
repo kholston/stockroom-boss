da7d5eced0a4600d957cba2b8329d7f02b6771bf
Ken Holston <git.kholston@gmail.com>
2022-03-06 14:49:46 -0500

    move tests and update formatter

--- api/src/tests/server.test.ts
+++ api/src/tests/server.test.ts
@@ -8,7 +8,10 @@ describe('Basic Server Test', () => {
   describe('Once Server is running', () => {
     // Checks /health endpoint to see if the server is running
     test('health check returns ok', async () => {
-      const { text } = await api.get('/health').expect(200).expect('Content-Type', /text\/html/);
+      const { text } = await api
+        .get('/health')
+        .expect(200)
+        .expect('Content-Type', /text\/html/);
       expect(text).toEqual('ok');
     });
   });
