12387b5762e10cdae90982b6835e1b93e8be6719
Ken Holston <git.kholston@gmail.com>
2022-03-07 13:55:21 -0500

    binding host

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ import app from './app';
 
 const server: http.Server = http.createServer(app);
 
-server.listen(config.PORT, () =>
+server.listen([config.PORT,'0.0.0.0'], () =>
   console.info(`Server running on port ${config.PORT}`)
 );
