2f443483a6b995491f4b8fd2ec24627b044a5d0e
Ken Holston <git.kholston@gmail.com>
2022-03-07 13:12:04 -0500

    changed env config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,12 +2,11 @@ import { config } from 'dotenv';
 config(); // setup env variables
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI =
-  process.env.NODE_ENV === 'test'
-    ? process.env.MONGODB_TEST_URI
-    : process.env.MONGODB_URI;
+// const MONGODB_URI =
+//   process.env.NODE_ENV === 'test'
+//     ? process.env.MONGODB_TEST_URI
+//     : process.env.MONGODB_URI;
 
 export default {
-  PORT,
-  MONGODB_URI,
+  PORT
 };
